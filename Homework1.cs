43f9aeade3124efecbf7e137d9732a4ce54c92f7
rannikhal <97406052+rannikhal@users.noreply.github.com>
2023-01-22 15:53:09 -0600

    Homework1 code

---
+++ Homework1.cs
@@ -0,0 +1,11 @@
+namespace Homework1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 2.5;
+        double y = 3.3;
+        double z = (4*x*x)+(3*y);
+        Console.WriteLine($"X = {x}, Y = {y}\nThe value of Z is: {z}");
+    }
+}
