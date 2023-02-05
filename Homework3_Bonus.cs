4165581f493bec76285f30f4e612f941e990ac2a
rannikhal <97406052+rannikhal@users.noreply.github.com>
2023-02-05 12:15:12 -0600

    Add files via upload

---
+++ Homework3_Bonus.cs
@@ -0,0 +1,20 @@
+﻿namespace Homework_Bonus;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine ("Assign an int value to N:");
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        for (int row=1; row<=N; row++){
+            for(int col=1; col<=N; col++){
+                if (col<=(N-row)){
+                Console.Write(" ");
+                } else {
+                Console.Write(row);
+                }
+            }
+            Console.WriteLine("");
+        }
+    }
+}
